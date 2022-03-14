7dce296532d3a57a6b3b82ac9568615e62eb8ae3
haroon <91939783+haroonalbar@users.noreply.github.com>
2022-03-14 13:43:27 +0530

    Update FundMe.sol
    import aggregatorv3interfacae and safemath

--- FundMe.sol
+++ FundMe.sol
@@ -2,6 +2,9 @@
 
 pragma solidity >=0.6.6 <0.9.0;
 
+import "https://github.com/smartcontractkit/chainlink/blob/9d261f9555a4fe9ea4390ef86975c21f95ef7e3d/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "https://github.com/smartcontractkit/chainlink/blob/9d261f9555a4fe9ea4390ef86975c21f95ef7e3d/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
 contract FundMe{
   
   
